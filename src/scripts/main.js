6f89cc3bfd3a4d4f65abdc264f49f6db5ecbebab
Olexandr Ivaschenko <sasaivasenko9@gmail.com>
2025-04-24 21:16:11 +0300

    create img event

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const largeImage = document.getElementById('largeImg');
+
+const galleryList = document.querySelector('.gallery__list');
+
+galleryList.addEventListener('click', (e) => {
+  const target = e.target.closest('li');
+
+  if (!target) {
+    return;
+  }
+
+  const imgSrc = target.querySelector('img').src;
+
+  largeImage.src = imgSrc;
+});
